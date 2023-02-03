72789bec043908c63a84fe8f2b55d63f1f704fac
azurekurs23 <124287271+azurekurs23@users.noreply.github.com>
2023-02-03 06:22:50 +0100

    2

--- tabelle.php
+++ tabelle.php
@@ -1,6 +1,5 @@
 <html> <!-- Start HTML -->
 <head> <!-- Start HTML-Kopf -->
-
 <!-- CSS-Formatierung für die Tabelle -->
 <style type="text/css"> 
  table { 
@@ -8,7 +7,6 @@
  border-collapse: collapse; 
  border: none; 
  } 
-
  thead { 
  background-color: #4D79C7; 
  color: #FFFFFF; 
@@ -18,25 +16,20 @@
  text-align: left; 
  padding: 0.5em 1em; 
  } 
-
 </style> <!-- Ende CSS-Style -->
 </head> <!-- Ende HTML-Kopf -->
 <body> <!-- Start HTML-Body --> 
-
 <?php //Start PHP
-
 //Parameter holen und ausgeben 
 $bnr = $_GET["bnr"]; 
 echo "<h1>iBrot-Bestellung</h1>"; 
 echo "<h2>Bestellnummer: $bnr </h2>"; 
-
 /* Daten in einem assoziativen Array definieren*/
 $daten = array ( 
  array("Position"=>1,"ArtikelID"=>"Brötchen","Menge"=>5), 
  array("Position"=>2,"ArtikelID"=>"Croissant","Menge"=>3), 
  array("Position"=>3,"ArtikelID"=>"Brot","Menge"=>1) 
  ); 
-
 /* Überschriften im Tabellen-Head ausgeben */
 echo "<table>"; 
 echo "<thead>"; 
@@ -46,7 +39,6 @@ echo "<th>Artikel-ID</th>";
 echo "<th>Menge</th>"; 
 echo "</tr>"; 
 echo "</thead>"; 
-
 /* Datensätze im Tabellen-Body ausgeben */
 echo "<tbody>"; 
 foreach ($daten as $row) { 
@@ -56,7 +48,6 @@ foreach ($daten as $row) {
  echo "<td>".$row['Menge']."</td>"; 
  echo "</tr>"; 
 } 
-
 echo "</table>"; 
 echo "<tbody>"; 
  
